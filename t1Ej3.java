86b01eaed17e43c657673c187dba340064fbf10e
ArielHernan <72375204+ArielHernan@users.noreply.github.com>
2020-10-05 10:00:22 +0200

    Add files via upload

---
+++ t1Ej3.java
@@ -0,0 +1,24 @@
+/*
+ * Ejercicio 3
+Escribe un programa que muestre por pantalla 10 palabras en inglés junto a su
+correspondiente traducción al castellano. Las palabras deben estar distribuidas
+en dos columnas y alineadas a la izquierda.
+
+Ariel Rojas
+ */
+
+public class t1Ej3 {
+	
+	public static void main (String args[]) {
+    System.out.printf("%-10s %-6s\n","casa","house");
+    System.out.printf("%-10s %-6s\n","árbol","tree");
+    System.out.printf("%-10s %-6s\n","coche","car");
+    System.out.printf("%-10s %-6s\n","mesa","table");
+    System.out.printf("%-10s %-6s\n","silla","chear");
+    System.out.printf("%-10s %-6s\n","arbol","tree");
+    System.out.printf("%-10s %-6s\n","niño","kid");
+    System.out.printf("%-10s %-6s\n","hogar","home");
+		
+	}
+}
+
---
+++ t1Ej9.java
@@ -0,0 +1,28 @@
+/*
+    Ejercicio 9
+    Escribe un programa que pinte por pantalla alguna escena - el campo, la
+    habitación de una casa, un aula, etc. - o algún objeto animado o inanimado
+    - un coche, un gato, una taza de café, etc. Ten en cuenta que puedes utilizar
+    caracteres como *, +, <, #, @, etc. o incluso caracteres Unicode. ¡Échale
+    imaginación!
+ * 
+ */
+public class t1Ej9 {
+	
+	public static void main (String args[]) {
+    
+  System.out.println("\033[35m                 ****");
+  System.out.println("                   *          *   *          *     *      ");
+  System.out.println("                   *         **    *        *     * *     ");
+  System.out.println("                * ****      *  *    *      *     *   *    ");
+  System.out.println("               *   *       ******    *    *     *******   ");
+  System.out.println("                *  *      *     *     *  *     *       *  ");
+  System.out.println("                 * *     *       *     **     *         * ");
+
+
+
+
+
+	}
+}
+
