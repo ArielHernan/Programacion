c8c63571004ad9942b2ec71d740df4d3779b36a6
ArielHernan <arielhernan.rojas.alu@iescampanillas.com>
2020-10-20 12:50:05 +0200

    actualizado durante clase

--- Ejercicios_en_clase/ej4ClaseT4.java
+++ Ejercicios_en_clase/ej4ClaseT4.java
@@ -3,28 +3,42 @@
  * 
  * Copyright 2020 Ariel Rojas <arielhernan.rojas.alu@iescampanillas.com>
  * 
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
- * MA 02110-1301, USA.
- * 
- * 
+  *Programa que escriba por pantalla la tabla de potencias de un número 
+  * introducido por teclado.
+  * También se ha de pedir por teclado la cdntidad de filas que 
+  * ha de tener la tabla de potencias .
+  * La tebla de potencias empezará por el primer número  elevado a la primera potencia (1) ,
+  * después lo hará elevado a la segunda
+  * potencia (2) y así hasta llegar al segundo número.
+  * 
  */
 
-
+import java.util.Scanner;
 public class ej4ClaseT4 {
 	
 	public static void main (String args[]) {
+    
+    int n=0;
+    int filas=0;
+    int r = 1;
+    Scanner s = new Scanner(System.in);
+    
+    System.out.println("Este programa calcula la potencia de un número");
+    
+    System.out.println("introduce el número:");
+    n= s.nextInt();
+    
+    System.out.println("introduce las filas:");
+    filas= s.nextInt();
+    
+    System.out.println(" ");
+    
+    for(int i =1;i<=filas;i++){
+      r=n*r;
+      System.out.println(r);
+      }
+
+    
 		
 	}
 }
