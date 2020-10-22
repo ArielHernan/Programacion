7ae13452f3482c863ac7a8daf5801d7b246330fa
ArielHernan <arielhernan.rojas.alu@iescampanillas.com>
2020-10-22 16:11:05 +0200

    actualizacion

---
+++ EjerciciosDelLibro/Tema 5/ejClasePiramide2.java
@@ -0,0 +1,23 @@
+/*
+ * ejClasePiramide2.java
+ * 
+ * Copyright 2020 Ariel Rojas <arielhernan.rojas.alu@iescampanillas.com>
+ * 
+    A partir de un número introducido por teclado (ej:6)
+              1
+            1 2 3
+          1 2 3 4 5
+        1 2 3 4 5 6 7
+      1 2 3 4 5 6 7 8 9
+    1 2 3 4 5 6 7 8 9 0 1
+ * 
+ */
+
+
+public class ejClasePiramide2 {
+	
+	public static void main (String args[]) {
+		
+	}
+}
+
---
+++ Ejercicios_en_clase/ejPiramide2.java
@@ -0,0 +1,36 @@
+/*
+ * ejPiramide2.java
+ * 
+ * Copyright 2020 Ariel Rojas <arielhernan.rojas.alu@iescampanillas.com>
+ * 
+    A partir de un número introducido por teclado (ej:6)
+              1
+            1 2 3
+          1 2 3 4 5
+        1 2 3 4 5 6 7
+      1 2 3 4 5 6 7 8 9
+    1 2 3 4 5 6 7 8 9 0 1
+
+         1
+        123
+       12345
+      1234567
+     123456789 
+    12345678901
+
+         1
+        121
+       12321
+      1234321
+     123454321
+    12345654321
+ */
+
+
+public class ejPiramide2 {
+	
+	public static void main (String args[]) {
+		
+	}
+}
+
