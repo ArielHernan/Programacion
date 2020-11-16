667a04ca06d59d04a6906bbc493e4e656a6adc9c
ArielHernan <arielhernan.rojas.alu@iescampanillas.com>
2020-11-16 17:08:27 +0100

    actualizacion

---
+++ EjerciciosDelLibro/Tema 7/tema7Ej1.java
@@ -0,0 +1,32 @@
+/*
+ * tema7Ej1.java
+ * 
+ * Copyright 2020 Ariel Rojas <arielhernan.rojas.alu@iescampanillas.com>
+ * 
+    Define un array de 12 números enteros con nombre num y asigna los valores
+    según la tabla que se muestra a continuación. Muestra el contenido de todos
+    los elementos del array. ¿Qué sucede con los valores de los elementos que no
+    han sido inicializados?
+
+ */
+
+
+public class tema7Ej1 {
+	
+	public static void main (String args[]) {
+		
+    
+    int []num=new int[12];
+    num[0]=39;
+    num[1]=-2;
+    num[4]=0;
+    num[6]=14;
+    num[8]=5;
+    num[9]=120;
+    for(int i=0;i<12;i++){
+    System.out.println(num[i]);
+    }
+    
+	}
+}
+
